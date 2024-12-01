b5fc68e74473e56afa6756253c26aee60395046c
djbiel2 <129142671+djbiel2@users.noreply.github.com>
2024-12-01 02:40:40 -0600

    Update My_LLM.scala

--- src/main/scala/My_LLM.scala
+++ src/main/scala/My_LLM.scala
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContextExecutor
 
-
 object My_LLM extends App {
   implicit val system: ActorSystem = ActorSystem("DawidLLM")
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
@@ -22,7 +21,8 @@ object My_LLM extends App {
   // Load the trained model
   val word2VecModel: WordVectors = WordVectorSerializer.readWord2VecModel(model_out_path)
   logger.info("Model loaded successfully")
-  //serve html page
+
+  // Serve HTML page
   val route =
     path("") {
       get {
@@ -40,7 +40,8 @@ object My_LLM extends App {
           }
         }
       }
-  // generate a response
+
+  // Generate a response
   def response_generator(query: String): String = {
     try {
       val words = query.split(" ")
@@ -59,7 +60,11 @@ object My_LLM extends App {
     }
   }
 
-  //starting server
-  Http().newServerAt("0.0.0.0", 80).bind(route)
-  logger.info("Server successfuly started")
+  // Starting server
+  Http().newServerAt("0.0.0.0", 80).bind(route).onComplete {
+    case scala.util.Success(binding) =>
+      logger.info(s"Server successfully started at ${binding.localAddress}")
+    case scala.util.Failure(exception) =>
+      logger.error("Failed at binding server", exception)
+  }
 }
